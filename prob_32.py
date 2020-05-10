52cecbc48dc26dbbebf406b97f20e97d41803e13
Hrishikesh Mulkutkar <51927760+Hrishikesh-3459@users.noreply.github.com>
2020-05-10 08:54:23 +0530

    Added solution to problem 32

---
+++ prob_32.py
@@ -0,0 +1,12 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import re
+x = int(input())
+
+for i in range(x):
+    try:
+        re.compile(input())
+        is_valid = True
+    except re.error:
+        is_valid = False
+    print(is_valid)
+
