9895f317678494b5a0f198d6635e905c60c06b1f
Hrishikesh Mulkutkar <51927760+Hrishikesh-3459@users.noreply.github.com>
2020-05-10 08:51:22 +0530

    Added solution to problem 28

---
+++ prob_28.py
@@ -0,0 +1,22 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from collections import Counter
+
+x = int(input())
+
+shoes = list(map(int, input().rstrip().split()))
+count_dict = Counter(shoes)
+
+n = int(input())
+
+profit = 0
+
+for i in range(n):
+    cur = list(map(int, input().rstrip().split()))
+    if (count_dict[cur[0]] != 0):
+        profit += cur[1]
+        count_dict[cur[0]] -= 1
+
+
+print(profit)
+
+
