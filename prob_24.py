b0efd07e39e75f1867146b68b7a3a61c84678edd
Hrishikesh Mulkutkar <51927760+Hrishikesh-3459@users.noreply.github.com>
2020-05-10 08:48:09 +0530

    Added solution to problem 24

---
+++ prob_24.py
@@ -0,0 +1,17 @@
+import textwrap
+
+def wrap(string, max_width):
+    s = ""
+    for i in range(len(string)):
+        if (i % max_width == 0 and i != 0):
+            s += "\n"
+        s += string[i] 
+    return (s)
+
+        
+
+
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
